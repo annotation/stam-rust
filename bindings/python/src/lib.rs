4dd540deffca591132ce0b1006185e641e9a27dd
Maarten van Gompel <proycon@anaproy.nl>
2023-02-01 15:38:11 +0100

    further implementation of python binding #1

--- bindings/python/src/lib.rs
+++ bindings/python/src/lib.rs
@@ -2,7 +2,7 @@ extern crate stam as libstam;
 
 use pyo3::create_exception;
 use pyo3::exceptions::{PyException, PyIndexError, PyKeyError, PyRuntimeError, PyValueError};
-use pyo3::ffi::{PyLong_GetInfo, PyLong_Type};
+use pyo3::ffi::{PyLong_GetInfo, PyLong_Type, Py_None};
 use pyo3::prelude::*;
 use pyo3::types::*;
 use std::ops::FnOnce;
@@ -146,6 +146,32 @@ impl PyAnnotationDataSet {
     fn to_file(&self, filename: &str) -> PyResult<()> {
         self.map(|annotationset| annotationset.to_file(filename))
     }
+
+    /// Get a DataKey instance by ID, raises an exception if not found
+    fn key(&self, key: &str) -> PyResult<PyDataKey> {
+        self.map(|annotationset| {
+            annotationset
+                .resolve_key_id(key)
+                .map(|keyhandle| PyDataKey {
+                    set: self.handle,
+                    handle: keyhandle,
+                    store: self.store.clone(),
+                })
+        })
+    }
+
+    /// Get a AnnotationData instance by id, raises an exception if not found
+    fn annotationdata(&self, data_id: &str) -> PyResult<PyAnnotationData> {
+        self.map(|annotationset| {
+            annotationset
+                .resolve_data_id(data_id)
+                .map(|datahandle| PyAnnotationData {
+                    set: self.handle,
+                    handle: datahandle,
+                    store: self.store.clone(),
+                })
+        })
+    }
 }
 
 impl PyAnnotationDataSet {
@@ -183,6 +209,12 @@ impl PyDataKey {
         self.map(|datakey| Ok(datakey.id().map(|x| x.to_owned())))
     }
 
+    /// Returns the public ID (by value, aka a copy)
+    /// Use this sparingly
+    fn __str__(&self) -> PyResult<Option<String>> {
+        self.map(|datakey| Ok(datakey.id().map(|x| x.to_owned())))
+    }
+
     /// Tests the ID of the dataset
     fn has_id(&self, other: &str) -> PyResult<bool> {
         self.map(|datakey| Ok(datakey.id() == Some(other)))
@@ -206,6 +238,76 @@ impl PyDataKey {
                 .key(&self.handle.into())
                 .ok_or_else(|| PyRuntimeError::new_err("Failed to resolved annotationset"))?;
             f(datakey).map_err(|err| PyStamError::new_err(format!("{}", err)))
+        } else {
+            Err(PyRuntimeError::new_err(
+                "Unable to obtain store (should never happen)",
+            ))
+        }
+    }
+}
+
+#[pyclass(dict, name = "AnnotationData")]
+pub struct PyAnnotationData {
+    set: AnnotationDataSetHandle,
+    handle: AnnotationDataHandle,
+    store: Arc<RwLock<AnnotationStore>>,
+}
+
+fn datavalue_into_py<'py>(datavalue: &DataValue, py: Python<'py>) -> Result<&'py PyAny, StamError> {
+    match datavalue {
+        DataValue::String(s) => Ok(s.into_py(py).into_ref(py)),
+        DataValue::Float(f) => Ok(f.into_py(py).into_ref(py)),
+        DataValue::Int(v) => Ok(v.into_py(py).into_ref(py)),
+        DataValue::Bool(v) => Ok(v.into_py(py).into_ref(py)),
+        DataValue::Null => {
+            //feels a bit hacky, but I can't find a PyNone to return as PyAny
+            let x: Option<bool> = None;
+            Ok(x.into_py(py).into_ref(py))
+        }
+        DataValue::List(v) => {
+            let pylist = PyList::empty(py);
+            for item in v.iter() {
+                let pyvalue = datavalue_into_py(item, py)?;
+                pylist.append(pyvalue);
+            }
+            Ok(pylist)
+        }
+    }
+}
+
+#[pymethods]
+impl PyAnnotationData {
+    /// Returns a DataKey instance
+    fn key(&self) -> PyResult<PyDataKey> {
+        self.map(|annotationdata| {
+            Ok(PyDataKey {
+                set: self.set,
+                handle: annotationdata.key(),
+                store: self.store.clone(),
+            })
+        })
+    }
+
+    /// Returns the value (makes a copy)
+    /// In comparisons, use test_value() instead
+    fn value<'py>(&self, py: Python<'py>) -> PyResult<&'py PyAny> {
+        self.map(|annotationdata| datavalue_into_py(annotationdata.value(), py))
+    }
+}
+
+impl PyAnnotationData {
+    fn map<T, F>(&self, f: F) -> Result<T, PyErr>
+    where
+        F: FnOnce(&AnnotationData) -> Result<T, StamError>,
+    {
+        if let Ok(store) = self.store.read() {
+            let annotationset: &AnnotationDataSet = store
+                .annotationset(&self.set.into())
+                .ok_or_else(|| PyRuntimeError::new_err("Failed to resolved annotationset"))?;
+            let data: &AnnotationData = annotationset
+                .annotationdata(&self.handle.into())
+                .ok_or_else(|| PyRuntimeError::new_err("Failed to resolved annotationset"))?;
+            f(data).map_err(|err| PyStamError::new_err(format!("{}", err)))
         } else {
             Err(PyRuntimeError::new_err(
                 "Unable to obtain store (should never happen)",
