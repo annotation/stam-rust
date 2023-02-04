7eefee0efc71007b0c1299b479b11d15d7899d46
Maarten van Gompel <proycon@anaproy.nl>
2023-02-05 00:21:34 +0100

    cleanup

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -2,38 +2,38 @@
 
 import unittest
 
-import stam.stam
+from stam.stam import AnnotationStore, Offset, AnnotationData, AnnotationDataBuilder, Selector, SelectorKind, TextResource, DataKey, DataValue, AnnotationDataSet
 
 
 class Test1(unittest.TestCase):
     def setUp(self):
-        self.store = stam.AnnotationStore(id="test")
+        self.store = AnnotationStore(id="test")
         resource = self.store.add_resource(id="testres", text="Hello world")
         dataset = self.store.add_annotationset(id="testdataset")
         dataset.add_key("pos")
         data = dataset.add_data("pos","noun","D1")
         self.store.annotate(id="A1", 
-                            target=stam.Selector(kind=stam.SelectorKind.TEXTSELECTOR, offset=stam.Offset.simple(6,11), resource=resource),
-                            data=[stam.AnnotationDataBuilder.link(data)])
+                            target=Selector(kind=SelectorKind.TEXTSELECTOR, offset=Offset.simple(6,11), resource=resource),
+                            data=[AnnotationDataBuilder.link(data)])
 
     def test_sanity_1(self):
-        self.assertIsInstance( self.store, stam.AnnotationStore)
+        self.assertIsInstance( self.store, AnnotationStore)
         self.assertEqual(self.store.id, "test")
 
     def test_sanity_2(self):
         resource = self.store.resource("testres")
-        self.assertIsInstance( resource, stam.TextResource)
+        self.assertIsInstance( resource, TextResource)
         self.assertEqual(resource.id, "testres")
         self.assertTrue(resource.has_id("testres")) #quicker than the above (no copy)
 
     def test_sanity_3(self):
         dataset = self.store.annotationset("testdataset")
-        self.assertIsInstance( dataset, stam.AnnotationDataSet)
+        self.assertIsInstance( dataset, AnnotationDataSet)
         key = dataset.key("pos")
-        self.assertIsInstance( key, stam.DataKey)
+        self.assertIsInstance( key, DataKey)
         self.assertTrue(str(key), "pos")
         data = dataset.annotationdata("D1")
-        self.assertIsInstance( data, stam.AnnotationData)
+        self.assertIsInstance( data, AnnotationData)
         self.assertTrue(data.has_id("D1"))
 
 if __name__ == "__main__":
