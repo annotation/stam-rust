0252f53ca64c4222f6715f55f019f042f4e2e71d
Maarten van Gompel <proycon@anaproy.nl>
2023-06-15 13:06:13 +0200

    wip: big mess

--- src/search.rs
+++ src/search.rs
@@ -10,21 +10,190 @@ use smallvec::SmallVec;
 use std::iter::IntoIterator;
 use std::slice::Iter;
 
-pub enum Query<'q> {
-    /// Maps to to search_textselections call
-    TextSelections(Option<TextSelectionOperator>, Vec<Constraint<'q>>),
-    /// Maps to to search_annotations call
-    Annotations(Vec<Constraint<'q>>),
-    /// Maps to to search_resources call
-    Resources(Vec<Constraint<'q>>),
+pub struct Query<'store, 'q> {
+    store: &'store AnnotationStore,
+    subqueries: Vec<SelectQuery<'q>>,
+}
+
+pub struct SelectQuery<'q, T> {
+    /// Name for the Result Set
+    varname: Option<&'q str>,
+
+    //Constraints
+    constraints: Vec<Constraint<'q>>,
+}
+
+impl<T> SelectQuery<T> {
+    pub fn new() -> Self {
+        Self {
+            varname: None,
+            constraint: Vec::new(),
+        }
+    }
+    pub fn with_name(&mut self, name: &'q str) -> &mut Self {
+        self.varname = Some(name);
+        self
+    }
+    pub fn with_constraint(&mut self, constraint: Constraint<'q>) -> &mut Self {
+        self.constraints.push(constraint);
+        self
+    }
+    pub fn with_constraints(&mut self, constraints: Vec<Constraint<'q>>) -> &mut Self {
+        self.constraints = constraints;
+        self
+    }
+
+    /// Evaluate the Select Query (lazily!), will most-likely return a wrapped Iterator
+    /// Run collect() on the ResultSet to actually you get the results
+    pub fn eval(&self, context: &Context<'store, 'q>) -> ResultSet<T>;
+}
+
+impl SelectQuery<TextSelection> {
+    pub fn eval(&self, context: &Context<'store, 'q>) -> ResultSet<TextSelection> {
+        let store: &AnnotationStore = context.store;
+        let mut results: ResultSet<TextSelection> = ResultSet::Empty();
+        for constraint in constraints {
+            results = constraint.eval(&mut context)
+        }
+        results
+    }
+}
+
+pub struct Context<'store, 'q> {
+    store: &'store AnnotationStore,
+    varnames: Vec<&'q str>,
+    //v--- ID, resolved via varnames
+    textselections: Vec<(usize, ResultSet<TextSelection>)>,
+    annotations: Vec<(usize, ResultSet<Annotation>)>,
+    data: Vec<(usize, ResultSet<AnnotationData>)>,
+    resources: Vec<(usize, ResultSet<TextResource>)>,
+    datasets: Vec<(usize, ResultSet<AnnotationDataSet>)>,
+}
+
+impl<'store, 'q> Context<'store, 'q> {
+    pub fn new(store: &'store AnnotationStore) -> Self {
+        Self {
+            store: annotationstore,
+            varnames: Vec::new(),
+            textselections: Vec::new(),
+            annotations: Vec::new(),
+            data: Vec::new(),
+            resources: Vec::new(),
+            datasets: Vec::new(),
+        }
+    }
+}
+
+trait QueryIterator<'store, T> {
+    pub fn create(
+        context: Context<'store>,
+    ) -> Result<impl Iterator<Item = T::HandleType>, StamError>;
+}
+
+// Generic type for ResultSet
+pub enum ResultSet<T>
+where
+    T: Storable,
+{
+    Empty,
+    Vec(SmallVec<[T::HandleType; 8]>),
+    Iterator(Iterator<Item = T::HandleType>),
+    // MAYBE TODO: Let the selection set directly references a slice in the reverse index (there are no copies)?
+    //Borrowed(&'a [T::HandleType]),
+}
+
+impl<T> ResultSet<T>
+where
+    T: Storable,
+{
+    /// Consumes the result set, if it was an iterator, no-op if it already was
+    pub fn collect(mut self) -> Self {
+        match self {
+            Self::Vec(..) => self,
+            Self::Iterator(iter) => Self::Vec(iter.collect()),
+        }
+    }
+
+    /// Add an item to the result set
+    pub fn add<'q>(mut self, item: Item<'q, T>, store: &AnnotationStore) -> Self {
+        self = self.collect();
+        if let Some(handle) = item.to_handle(store) {
+            self.0.push(handle);
+        }
+        self
+    }
+}
+
+impl<T> ResultSet<T>
+where
+    T: Storable,
+{
+    pub fn iter(&self) -> Iter<T::HandleType> {
+        match self {
+            //Self::Borrowed(v) => v.iter(),
+            Self::Owned(v) => v.iter(),
+        }
+    }
+}
+
+impl<'store, 'q> Iterator for Query<'q> {
+    type Item = QueryResults<'store, 'q>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut popstack = false;
+        let mut newstackitem: Option<SearchStack> = None;
+        if let Some(stackitem) = self.stack.last() {
+            match stackitem {
+                SearchStack::Constraint(constraint) => {
+                    //obtain an iterator for the constraint and add it to the stack
+                    match constraint {
+                        Constraint::Annotation(selectionset) => {
+                            newstackitem = Some(SearchStack::AnnotationIter(selectionset.iter()));
+                            popstack = true;
+                        }
+                    }
+                }
+                SearchStack::AnnotationIter(iter) => {
+                    if let Some(annotationhandle) = iter.next() {
+                        let annotation: &Annotation = self.store.get(annotationhandle);
+                        self.store.textselections_by_annotation(annotationhandle);
+                    } else {
+                        //iterator is depleted
+                        popstack = true;
+                    }
+                }
+            }
+        }
+        if popstack {
+            self.stack.pop();
+        }
+        if let Some(newstackitem) = newstackitem {
+            self.stack.push(newstackitem);
+            self.next() //recurse
+        } else {
+            None
+        }
+    }
 }
 
 pub enum Constraint<'q> {
+    /// Constrains by annotations that have this associated data
+    /// The reference value to test against is embedded within the [`DataOperator`].
+    AnnotationData {
+        dataset: Item<'q, AnnotationDataSet>,
+        key: Item<'q, DataKey>,
+        operator: DataOperator<'q>,
+    },
+
+    TextRelation(TextSelectionOperator, &'q TextSelectionSet),
+
     /// Constrain by annotation selection set
     ///
     /// * When applied to annotation, this matches all annotations that are referenced by an annotation in the set
     /// * When applied to resources, textselections, or datasets, it constrains the annotations that reference that resource/textselection
     Annotation(&'q AnnotationSelectionSet),
+
+    AnnotationVar(&'q str),
 
     /// Constrain by annotation selection set
     ///
@@ -36,8 +205,12 @@ pub enum Constraint<'q> {
     /// When applied to annotation, this match all annotations that reference a resource in the set
     Resource(&'q ResourceSelectionSet),
 
-    /// Constrain by text selection set (known a priori, otherwise Query:TextSelections is used
-    TextSelection(Option<TextSelectionOperator>, &'q TextSelectionSet),
+    ResourceVar(&'q str),
+
+    /// Constrain by text selection set in subject position (known a priori, otherwise Query:TextSelections is used)
+    TextSelection(&'q TextSelectionSet, Option<TextSelectionOperator>),
+    /// Constrain by text selection set in object position (known a priori, otherwise Query:TextSelections is used)
+    TextSelectionInv(Option<TextSelectionOperator>, &'q TextSelectionSet),
 
     /// Annotation Set
     AnnotationDataSet(Item<'q, AnnotationDataSet>),
@@ -53,14 +226,6 @@ pub enum Constraint<'q> {
         operator: Option<TextSelectionOperator>,
     },
     */
-    /// Constrains by annotations that have this associated data
-    /// The reference value to test against is embedded within the [`DataOperator`].
-    AnnotationData {
-        dataset: Item<'q, AnnotationDataSet>,
-        key: Item<'q, DataKey>,
-        operator: DataOperator<'q>,
-    },
-
     /// Constrains by annotations that use this key (regardless of value)
     DataKey {
         dataset: Item<'q, AnnotationDataSet>,
@@ -77,55 +242,6 @@ pub enum Constraint<'q> {
     Not(Box<Constraint<'q>>),
 }
 
-// Generic type for SelectionSets (not used by TextSelectionSet)
-#[derive(Clone)]
-pub enum SelectionSet<T>
-where
-    T: Storable,
-{
-    // TODO: Let the selection set directly references a slice in the reverse index (there are no copies)
-    //Borrowed(&'a [T::HandleType]),
-    Owned(SmallVec<[T::HandleType; 8]>),
-}
-
-type AnnotationSelectionSet = SelectionSet<Annotation>;
-type DataSetSelectionSet = SelectionSet<AnnotationDataSet>;
-type ResourceSelectionSet = SelectionSet<TextResource>;
-//type DataSelectionSet<'a> = SelectionSet<'a, AnnotationData>;
-
-impl<T> SelectionSet<T>
-where
-    T: Storable,
-{
-    /*
-    // TODO: use proper ToOwned trait
-    pub fn to_owned(mut self) -> Self {
-        match self {
-            Self::Borrowed(v) => self = Self::Owned(v.iter().map(|x| *x).collect()),
-            Self::Owned(v) => Self::Owned(v.iter().map(|x| *x).collect()),,
-        }
-    }
-    */
-
-    pub fn add<'q, 'store>(&mut self, item: Item<'q, T>, store: &'store AnnotationStore) {
-        if let Some(handle) = item.to_handle(store) {
-            self.0.push(handle);
-        }
-    }
-}
-
-impl<T> SelectionSet<T>
-where
-    T: Storable,
-{
-    pub fn iter(&self) -> Iter<T::HandleType> {
-        match self {
-            //Self::Borrowed(v) => v.iter(),
-            Self::Owned(v) => v.iter(),
-        }
-    }
-}
-
 /*
 impl<'a, T> IntoIterator for SelectionSet<'a, T> {
     type Item = T;
@@ -140,11 +256,8 @@ impl<'a, T> IntoIterator for SelectionSet<'a, T> {
 */
 
 impl AnnotationStore {
-    pub fn search_textselections<'store, 'q>(
-        &'store self,
-        constraints: &'q [Constraint<'q>],
-    ) -> SearchTextSelectionIter<'store, 'q> {
-        SearchTextSelectionIter {
+    pub fn query<'store, 'q>(&'store self, query: Query<'store, 'q>) -> QueryIter<'store, 'q> {
+        QueryIter {
             store: self,
             stack: constraints
                 .iter()
@@ -154,8 +267,9 @@ impl AnnotationStore {
     }
 }
 
-pub struct SearchTextSelectionIter<'store, 'q> {
+pub struct QueryIter<'store, 'q> {
     store: &'store AnnotationStore,
+
     stack: SmallVec<[SearchStack<'q>; 10]>,
 }
 
@@ -165,8 +279,33 @@ enum SearchStack<'q> {
     TextSelectionIter(Iter<'q, TextSelection>),
 }
 
-impl<'store, 'q> Iterator for SearchTextSelectionIter<'store, 'q> {
-    type Item = (TextSelection, TextResourceHandle, AnnotationHandle);
+pub enum QueryResult<'store, 'q> {
+    Annotation(Option<&'q str>, &'store Annotation),
+    Resource(Option<&'q str>, &'store Resource),
+    TextSelection(Option<&'q str>, &'store TextSelection), //may need to be TextSelections with a SmallVec
+}
+
+pub struct QueryResults<'store, 'q> {
+    row: Vec<QueryResult<'q, 'store>>,
+}
+
+impl<'q, 'store> QueryResults<'q, 'store> {
+    pub fn annotation(var: &str) -> Option<&'store Annotation> {
+        for field in self.row.iter() {
+            if let QueryResult::Annotation(var2, annotation) = field {
+                if var == var2 {
+                    return Some(annotation);
+                }
+            }
+        }
+        None
+    }
+    pub fn textselection(var: &str) -> Option<&'store TextSelection> {}
+    pub fn resource(var: &str) -> Option<&'store Resource> {}
+}
+
+impl<'store, 'q> Iterator for QueryIter<'store, 'q> {
+    type Item = QueryResults<'store, 'q>;
 
     fn next(&mut self) -> Option<Self::Item> {
         let mut popstack = false;
