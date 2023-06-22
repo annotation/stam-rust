6811ac732fab2c2118c21d8944f21e5272783876
Maarten van Gompel <proycon@anaproy.nl>
2023-06-22 10:34:31 +0200

    added a note

--- src/textselection.rs
+++ src/textselection.rs
@@ -1759,6 +1759,9 @@ trait FindTextSelections<'store> {
                     .range(reftextselection.end(), self.resource().textlen())
             }
             TextSelectionOperator::Overlaps { .. } | TextSelectionOperator::Embedded { .. } => {
+                //this is more efficient for reference text selections at the beginning of a text than at the end
+                //(we could reverse the iterator to more efficiently find fragments more at the end, but then we have bookkeeping to do to also revert
+                //the results, as they need to be in textual order)
                 self.resource().range(0, reftextselection.end())
             }
             _ => self.resource().iter(), //return the maximum slice
