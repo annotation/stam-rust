a7ae182d0d77e19e2277745ef187bc7c669cd7a9
Maarten van Gompel <proycon@anaproy.nl>
2023-09-21 21:55:15 +0200

    intersectioniter can also take an iterator (as first source)

--- src/lib.rs
+++ src/lib.rs
@@ -55,7 +55,7 @@ pub use regex::{Regex, RegexSet};
 
 mod tests;
 
-use smallvec::SmallVec;
+use smallvec::{smallvec, SmallVec};
 use std::borrow::Cow;
 
 // Lazy iterator computing an intersection
@@ -66,6 +66,9 @@ where
 {
     sources: SmallVec<[IntersectionSource<'a, T>; 2]>,
     cursors: SmallVec<[usize; 2]>,
+
+    /// indicates that no results can be expected anymore
+    abort: bool,
 }
 
 pub(crate) struct IntersectionSource<'a, T>
@@ -73,8 +76,31 @@ where
     T: Ord,
     [T]: ToOwned<Owned = Vec<T>>,
 {
-    data: Cow<'a, [T]>,
+    iter: Option<Box<dyn Iterator<Item = T>>>,
+    array: Option<Cow<'a, [T]>>,
     sorted: bool,
+}
+
+impl<'a, T> IntersectionSource<'a, T>
+where
+    T: Ord,
+    [T]: ToOwned<Owned = Vec<T>>,
+{
+    fn len(&self) -> Option<usize> {
+        if let Some(array) = self.array {
+            return Some(array.len());
+        }
+        None
+    }
+
+    fn take_item(&mut self, index: usize) -> Option<T> {
+        if let Some(array) = self.array {
+            return array.get(index).map(|x| *x);
+        } else if let Some(iter) = self.iter.as_mut() {
+            return iter.next();
+        }
+        return None;
+    }
 }
 
 impl<'a, T> IntersectionIter<'a, T>
@@ -86,12 +112,35 @@ where
         let mut iter = Self {
             sources: SmallVec::new(),
             cursors: SmallVec::new(),
+            abort: false,
         };
         iter.with(source, sorted)
     }
 
+    pub(crate) fn new_with_iterator(iter: Box<dyn Iterator<Item = T>>, sorted: bool) -> Self {
+        let source = IntersectionSource {
+            array: None,
+            iter: Some(iter),
+            sorted,
+        };
+        Self {
+            cursors: smallvec!(0),
+            sources: smallvec!(source),
+            abort: false,
+        }
+    }
+
     pub(crate) fn with(mut self, data: Cow<'a, [T]>, sorted: bool) -> Self {
-        let source = IntersectionSource { data, sorted };
+        if data.is_empty() {
+            //don't bother, empty data invalidates the whole iterator
+            self.abort = true;
+            return self;
+        }
+        let source = IntersectionSource {
+            array: Some(data),
+            iter: None,
+            sorted,
+        };
         if self.sources.is_empty() {
             self.sources.push(source);
             self.cursors.push(0);
@@ -99,8 +148,9 @@ where
             //find insertion point
             let mut pos = 0;
             for refsource in self.sources.iter() {
-                if (source.sorted == refsource.sorted) && (refsource.data.len() > source.data.len())
-                {
+                if refsource.array.is_none() {
+                    //pass: arrays go at the end
+                } else if (source.sorted == refsource.sorted) && (refsource.len() > source.len()) {
                     //shorter before longer (allows us to discard quicker)
                     break;
                 } else if !source.sorted && refsource.sorted {
@@ -121,30 +171,29 @@ where
     }
 }
 
-impl<'a, T> Iterator for IntersectionIter<'a, T>
+impl<'a, T> IntersectionIter<'a, T>
 where
-    T: Ord + Copy,
+    T: Ord,
+    [T]: ToOwned<Owned = Vec<T>>,
 {
-    type Item = T;
-    fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            let left = self.sources.get(0).unwrap();
-            if let Some(item) = left.data.get(self.cursors[0]).copied() {
-                if self.sources[1..].is_empty() {
-                    //only one source
-                    self.cursors[0] += 1;
-                    return Some(item);
-                } else {
-                    let mut found = true; //falsify
-                    for (i, right) in self.sources.iter().enumerate() {
-                        if i == 0 {
-                            //the first item is the left iter
-                            continue;
-                        };
-                        if right.sorted {
-                            //binary search
-                            if let Ok(index) = right.data[self.cursors[i]..].binary_search(&item) {
-                                if left.sorted {
+    fn test_sources(&mut self, item: T, left_sorted: bool) -> Option<T> {
+        if self.sources[1..].is_empty() {
+            //only one source
+            self.cursors[0] += 1;
+            return Some(item);
+        } else {
+            let mut found = true; //falsify
+            for (i, right) in self.sources.iter().enumerate() {
+                if i == 0 {
+                    //the first item is the left iter
+                    continue;
+                };
+                if let Some(data) = right.array {
+                    if right.sorted {
+                        //binary search
+                        match data[self.cursors[i]..].binary_search(&item) {
+                            Ok(index) => {
+                                if left_sorted {
                                     // the left is sorted so we can discount 'lesser than' items from the right side
                                     // in subsequent iterations by moving the cursor ahead
                                     // this is an extra optimisation
@@ -152,24 +201,55 @@ where
                                 }
                                 //item found! continue with next source to ensure item is also in there
                                 continue;
-                            } else {
-                                found = false;
-                                break;
                             }
-                        } else {
-                            //linear search
-                            if !right.data.contains(&item) {
+                            Err(index) => {
                                 found = false;
+                                if left_sorted && index == data.len() {
+                                    // because the left hand side is sorted and we're at the end of this right array,
+                                    // we can abort the whole IntersectionIter, no more results will be found
+                                    // this is an extra optimisation
+                                    self.abort = true;
+                                }
                                 break;
                             }
                         }
+                    } else {
+                        //linear search
+                        if !data.contains(&item) {
+                            found = false;
+                            break;
+                        }
                     }
-                    if found {
-                        self.cursors[0] += 1;
-                        return Some(item);
-                    }
+                } else {
+                    unreachable!("IntersectionIter: only the first source may be an interator");
                 }
-                //no results found, continue with next item
+            }
+            if found {
+                self.cursors[0] += 1;
+                return Some(item);
+            }
+        }
+        None
+    }
+}
+
+impl<'a, T> Iterator for IntersectionIter<'a, T>
+where
+    T: Ord + Copy,
+{
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let left = self.sources.get_mut(0).unwrap();
+            if self.abort {
+                return None;
+            } else if let Some(item) = left.take_item(self.cursors[0]) {
+                //                                     ^-- (cursor may or may not be used depending on source type)
+                let result = self.test_sources(item, left.sorted);
+                if result.is_some() {
+                    return result;
+                }
+                //no results found, continue with next item (cursor may or may not be used)
                 self.cursors[0] += 1;
             } else {
                 return None;
